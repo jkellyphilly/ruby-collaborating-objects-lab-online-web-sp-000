127da503f9460bd059d5b25af97e75467ad1b492
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-01 17:16:33 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,7 +11,7 @@ class MP3Importer
   def files
     my_files = Dir.entries(@path) #.select {|file| file.scan(/[.]\w+/) == ".mp3"}
     return_files = []
-    my_files.each_with_index do |file|
+    my_files.each do |file|
       binding.pry
       if file.scan(/[.]\w+/) == ".mp3"
         my_split = file.split(/[.]\w+/)
